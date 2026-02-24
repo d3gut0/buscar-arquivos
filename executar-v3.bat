672f40dfd752ca9b25085d4a87479aff30713987
Denisson Silva <denisson.silva@grupozanotti.com.br>
2026-02-24 08:25:25 -0300

    v3

---
+++ executar-v3.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "%~dp0buscar-imagem-v3.ps1"
+pause
--- executar.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0buscar-imagem.ps1"
-pause
